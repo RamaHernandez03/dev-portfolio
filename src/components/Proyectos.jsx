43e174e4feb163ef3c391df1cddde85035cacc50
ramagammer <skwgrama@gmail.com>
2024-08-21 01:24:56 -0300

    Cambios Esteticos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -5,18 +5,24 @@ import js from '../assets/js.png';
 
 const ProjectCard = ({ image, title, description, link }) => {
     return (
-      <div className="bg-gray-800 rounded-lg p-6 shadow-[0_2px_2px_rgba(0,0,0,0.2)] hover:shadow-[0_12px_16px_rgba(0,0,0,0.2)] hover:translate-y-[-15px] transition-transform transition-shadow duration-500 w-full">
-        <a href={link} target="_blank" rel="noopener noreferrer"></a>
-        <img
-          src={image}
-          alt={title}
-          className="w-[300px] h-[300px] object-cover rounded-lg mb-4"
-        />
-        <h3 className="text-white text-xl text-center font-semibold mb-4">{title}</h3>
-        <p className="text-gray-400">{description}</p>
-      </div>
+      <a 
+        href={link} 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="block bg-gray-800 rounded-lg p-6 shadow-[0_2px_2px_rgba(0,0,0,0.2)] hover:shadow-[0_12px_16px_rgba(0,0,0,0.2)] hover:translate-y-[-15px] transition-transform transition-shadow duration-500 w-full"
+      >
+        <div className="flex justify-center mb-4">
+          <img
+            src={image}
+            alt={title}
+            className="w-[300px] h-[300px] object-cover rounded-lg"
+          />
+        </div>
+        <h3 className="text-white text-xl text-center font-semibold mt-6 mb-4">{title}</h3>
+        <p className="text-gray-400 text-center">{description}</p>
+      </a>
     );
-  };
+};
   
 
 const Projects = () => {
@@ -31,13 +37,13 @@ const Projects = () => {
         image: python,
         title: 'Proyecto 1',
         description: 'Descripción del proyecto 1.',
-        link: 'https://matesmood.com',
+        link: '#',
       },
       {
         image: js,
         title: 'Proyecto 1',
         description: 'Descripción del proyecto 1.',
-        link: 'https://matesmood.com',
+        link: '#',
       },
     ];
 
