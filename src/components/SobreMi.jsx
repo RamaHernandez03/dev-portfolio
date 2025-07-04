65f6398190a81b07b09cb4b1b2d45d017c162162
ramagammer <skwgrama@gmail.com>
2025-07-04 01:03:35 -0300

    New photo

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import { useInView } from 'react-intersection-observer';
-import SobreMiImage from '../assets/me.jpeg';
+import SobreMiImage from '../assets/icon.jpg';
 
 const SobreMi = () => {
   const { ref, inView } = useInView({
